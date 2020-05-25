e38b1dfadf92e5b6b2a147bcc733058f80311836
georgeanthony33 <georgeanthony33@gmail.com>
2020-05-25 15:55:00 +0100

    added contacts to navbar

--- frontend/src/components/common/NavBar.js
+++ frontend/src/components/common/NavBar.js
@@ -25,28 +25,49 @@ const NavBar = () => {
   }, []);
 
   return (
-    <nav className="navbar is-dark is-size-5 is-fixed-top">
-      <div className="container">
-        <div className="navbar-brand">
-          <Link className="navbar-item" to="/"><div className={scrollY < 45 ? 'logo' : 'logo small'}></div></Link>
-          <Link className="navbar-item" to="/sites/france">Sites France</Link>
-          <Link className="navbar-item" to="/sites/portugal">Sites Portugal</Link>
-          <a 
-            className={`navbar-burger ${navOpen ? 'is-active' : ''}`}
-            onClick={toggleNavbar}
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-        <div className={`navbar-menu ${navOpen ? 'is-active' : ''}`}>
-          <div className="navbar-end">
-            <Link className="navbar-item" to={`/profile`}>Bookings</Link>
+    <>
+      <nav className={scrollY < 45 ? 'navbar is-dark is-size-5 contacts-nav' : 'closed'}>
+        <div className="container">
+          <div className={`navbar-menu ${navOpen ? 'is-active' : ''}`}>
+            <div className="navbar-brand">
+              <a href="mailto:georgeanthony33@gmail.com">
+                <div class="contact-links">
+                  <i class="fas fa-envelope"></i>
+                  <p>sales@questenfrance.com</p>
+                </div>
+              </a>
+              <div class="contact-links">
+                <i class="fas fa-phone"></i>
+                <p>01204 415425 or 07985 093397</p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <nav className="navbar is-dark is-size-5 is-fixed-top">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link className="navbar-item" to="/"><div className={scrollY < 45 ? 'logo' : 'logo small'}></div></Link>
+            <Link className="navbar-item" to="/sites/france">Sites France</Link>
+            <Link className="navbar-item" to="/sites/portugal">Sites Portugal</Link>
+            <a 
+              className={`navbar-burger ${navOpen ? 'is-active' : ''}`}
+              onClick={toggleNavbar}
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div className={`navbar-menu ${navOpen ? 'is-active' : ''}`}>
+            <div className="navbar-end">
+              <Link className="navbar-item" to={`/profile`}>Bookings</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
   )
 }
  
