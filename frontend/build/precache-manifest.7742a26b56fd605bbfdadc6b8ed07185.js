ac11aa49fb14e46e75cfad1bbe806b9916d91af1
georgeanthony33 <georgeanthony33@gmail.com>
2020-06-01 23:15:51 +0100

    Ready for deployment

---
+++ frontend/build/precache-manifest.7742a26b56fd605bbfdadc6b8ed07185.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91184feec419f30d1ad7b4cc4c33167a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "906dec04613bdc45f653",
+    "url": "/static/css/2.ef624c9d.chunk.css"
+  },
+  {
+    "revision": "d23c10538fb6c786efae",
+    "url": "/static/css/main.c2ddbe61.chunk.css"
+  },
+  {
+    "revision": "906dec04613bdc45f653",
+    "url": "/static/js/2.2c6e6a2e.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.2c6e6a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23c10538fb6c786efae",
+    "url": "/static/js/main.89fa3a8e.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "326c24f068db1c90e9c906a5679d7368",
+    "url": "/static/media/CalicoPark.326c24f0.jpg"
+  },
+  {
+    "revision": "1e90200e895e71b4b0eb1e3d09b6bc2b",
+    "url": "/static/media/HomeBackground.1e90200e.jpg"
+  },
+  {
+    "revision": "0aef9ebff30109a0e37940575ff6f406",
+    "url": "/static/media/LOceano.0aef9ebf.jpg"
+  },
+  {
+    "revision": "4406b65623aaa4867ab1bd3c8dd8ed61",
+    "url": "/static/media/LesGenets.4406b656.jpg"
+  },
+  {
+    "revision": "7b63dd093b77440afb42fea23812035f",
+    "url": "/static/media/QeF.7b63dd09.png"
+  }
+]);
--- frontend/src/components/sites/SiteCard.js
+++ frontend/src/components/sites/SiteCard.js
@@ -11,7 +11,7 @@ const SiteCard = ({ name, id, main_image, short_description }) => {
           </div>
           <div className="card-image">
             <figure className="image">
-              {/* <img src={require(`../../assets${main_image}`)} alt={name} /> */}
+              <img src={process.env.PUBLIC_URL + '/logo192.png'} alt={name} />
             </figure>
           </div>
           <div className="card-content">
