97ad12acb5023d4723ce51b188b5b91382bd8545
georgeanthony33 <georgeanthony33@gmail.com>
2021-05-31 22:38:56 +0100

    Ready for deployment

--- project/settings/production.py
+++ project/settings/production.py
@@ -1,8 +1,7 @@
 from .base import *
-from decouple import config
 
 DEBUG = False
 
 SECRET_KEY = config('SECRET_KEY_PRODUCTION')
 
-# ALLOWED_HOSTS = ['https://quest-en-france.herokuapp.com/', ]
+ALLOWED_HOSTS = ['https://quest-en-france.herokuapp.com/', ]
