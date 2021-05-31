a2c4bf21d078b8546930b3bd3e9ddf00f044c538
georgeanthony33 <georgeanthony33@gmail.com>
2021-05-31 22:28:30 +0100

    Ready for deployment

--- project/settings/base.py
+++ project/settings/base.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
 from decouple import config
 import django_heroku
+import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- project/wsgi.py
+++ project/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.development')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.production')
 
 application = get_wsgi_application()
